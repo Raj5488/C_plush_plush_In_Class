5c0fae21ce93f34a41e95a50e6b06066dc632e61
JITU RAJ SHARMA <jiturajsharma8093@gmail.com>
2023-09-12 07:47:46 +0530

    Array_Start

---
+++ Array/Array1/syntaxAndDeclaration.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int arr[7]; // 0,1,2,3,4,5,6
+    // arr[0] = 7;
+    // arr[1] = 1;
+    // arr[2] = 6;
+    // arr[3] = 3;
+    // arr[4] = 9;
+    // arr[5] = 7;
+    // arr[6] = 0;
+    // cout<<arr[0];
+
+
+    // **********second methods
+    int arr[7] = {1,4,5,6,7,85,5};
+    cout<<arr[5];
+}
